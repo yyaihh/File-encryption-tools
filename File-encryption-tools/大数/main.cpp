6c04633a02efef09bcbdc62c30c7d82031c61615
YYaiHH <1903870154@qq.com>
2020-03-21 21:32:19 +0800

    提交

---
+++ File-encryption-tools/大数/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include"bignum.hpp"
+#include<string>
+using namespace std;
+int main(){
+    string num1;
+    string num2;
+    while(cin>>num1>>num2){
+        BigNum s1(num1);
+        BigNum s2(num2);
+        cout << num1 << " + " << num2 << " = " << s1 + s2 << endl;
+        cout << num1 << " - " << num2 << " = " << s1 - s2 << endl;
+        cout << num1 << " x " << num2 << " = " << s1 * s2 << endl;
+        cout << num1 << " / " << num2 << " = " << s1 / s2 << endl;
+        cout << num1 << " % " << num2 << " = " << s1 % s2 << endl;
+    }
+    return 0;
+}
